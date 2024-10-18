9528c0c597527b3706cf4de6883f41122909c0cc
Ashok <velmaashok765@gmail.com>
2024-10-18 04:46:59 +0000

    implement register

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express();
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const path = require("path");
-// const { v4: uuidv4 } = require("uuid");
-// const userId = uuidv4();
+const cors = require("cors");
+const { v4: uuidv4 } = require("uuid");
 const dbPath = path.join(__dirname, "userData.db");
 let port = process.env.PORT || 8000;
-
+app.use(cors());
 let db = null;
 const initializeDatabase = async () => {
   try {
@@ -26,15 +26,20 @@ const initializeDatabase = async () => {
 
 initializeDatabase();
 
+app.use(express.json());
+
 // POST request from user
-app.post("/sign/", async (req, res) => {
-  //   const { name, email, password } = req.body;
-  //   const query = `
-  //     insert into userData(id,name,email,password)
-  //     values(${1},'${name}','${email}','${password}')
-  //     `;
-  //   await db.run(query);
-  res.send(req.body);
+app.post("/sign", async (req, res) => {
+  console.log(req.body);
+  const { name, email, password } = req.body;
+  const id = uuidv4();
+  console.log(typeof id);
+  const query = `
+    insert into userData(id,name,email,password)
+    values('${id}','${name}','${email}','${password}')
+  `;
+  await db.run(query);
+  res.send("Successfully Registered");
 });
 
 app.get("/data", async (req, res) => {
